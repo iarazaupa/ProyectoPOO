3f6457942842b2169bd446b306e67f4070d5cd47
AlanJToledoB <alanjoelcapo@gmail.com>
2025-12-16 18:39:19 -0300

    agregando un par de cosas a la clase de producto

--- MiProyecto/main.cpp
+++ MiProyecto/main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 #include "Producto.h"
 
 int main (int argc, char *argv[]) {
-
+	Producto mateDeAlgarrobo(0, "mate de algarrobo", 16.00, 4);
+	
+	cout<<"el mate:"<<mateDeAlgarrobo.GetNombre()<<"cuesta"<<mateDeAlgarrobo.GetPrecio()<<"y hay:"<<mateDeAlgarrobo.GetStock()<<"unidades";
+	
+	
 	return 0;
 }
 
