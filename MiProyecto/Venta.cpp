78df84aa374e2c7b84934977025be027f67f55bb
iarazaupa <iarazaupa0123@gmail.com>
2025-12-22 20:38:21 -0300

    agregue VENTA.cpp e .h cambien el main pero para ejecutarlo necesito DetalleVenta

---
+++ MiProyecto/Venta.cpp
@@ -0,0 +1,105 @@
+#include "Venta.h"
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+//constructor
+
+Venta::Venta(int ID, Cliente* cliente) {
+	m_ID = ID;
+	m_cliente = cliente;
+	m_total = 0;
+	m_archivoVentas = "ventas.dat";
+}
+
+
+//agrego producto
+
+bool Venta::AgregarProducto(Producto* producto, int cantidad) {
+	
+	if (producto == NULL || cantidad <= 0) {
+		return false;
+	}
+	
+	DetalleVenta d(producto, cantidad);
+	m_detalles.push_back(d);
+	
+	return true;
+}
+
+
+//calculo total
+
+void Venta::CalcularTotal() {
+	
+	m_total = 0;
+	
+	for(int i = 0; i < m_detalles.size(); i++) {
+		m_total += m_detalles[i].GetSubtotal();
+	}
+}
+
+
+//confirmo venta
+
+void Venta::ConfirmarVenta() {
+	
+	CalcularTotal();
+	GuardarEnArchivos();
+}
+
+
+// getters
+
+double Venta::Gettotal(){
+	return m_total;
+}
+
+int Venta::GetID(){
+	return m_ID;
+}
+
+string Venta::Getfecha(){
+	return m_fecha;
+}
+
+
+//guardo en el archivo
+
+void Venta::GuardarEnArchivos() {
+	
+	ofstream archivo(m_archivoVentas.c_str(), ios::app | ios::binary);
+	
+	if(archivo){
+		archivo.write((char*)&m_ID, sizeof(int));
+		
+		int idCliente = m_cliente->getID();
+		archivo.write((char*)&idCliente, sizeof(int));
+		
+		archivo.write((char*)&m_total, sizeof(double));
+	}
+	
+	archivo.close();
+}
+
+
+//mostrar ticket
+
+string Venta::MostrarTicket(){
+	
+	stringstream comprobante;
+	
+	comprobante<< "venta id: " << m_ID<<endl;
+	comprobante<< "cliente: " << m_cliente->getNombre()<<endl;
+	comprobante<< "----------------------\n";
+	
+	for(int i = 0; i < m_detalles.size(); i++){
+		comprobante<<m_detalles[i].GetDescripcion()<<endl;
+	}
+	
+	comprobante<<"total: $"<<m_total<<endl;
+	
+	return comprobante.str();
+}
